a7c14872fbd1b82d421e0ecf717da5fc13e0d241
Olesia <ivanova.ov_87@mail.ru>
2021-11-30 14:44:39 +0300

    new file:   .gitignore 	modified:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/components/About/About.js 	new file:   src/components/About/About.module.css 	new file:   src/components/About/img/group4.svg 	new file:   src/components/About/img/spinner-1s-200px.gif 	new file:   src/components/App/App.js 	new file:   src/components/App/App.module.css 	new file:   src/components/App/img/logo.svg 	new file:   src/components/Contacts/Contacts.js 	new file:   src/components/Contacts/Contacts.module.css 	new file:   src/components/Contacts/img/email_icon.svg 	new file:   src/components/Contacts/img/facebook_icon.svg 	new file:   src/components/Contacts/img/github_icon.svg 	new file:   src/components/Contacts/img/instagram_icon.svg 	new file:   src/components/Contacts/img/telegram_icon.svg 	new file:   src/components/Contacts/img/vkontacte_icon.svg 	new file:   src/components/InputItem/InputItem.js 	new file:   src/components/InputItem/InputItem.module.css 	new file:   src/components/InputItem/img/plus.svg 	new file:   src/components/Item/Item.js 	new file:   src/components/Item/Item.module.css 	new file:   src/components/Item/img/checked.svg 	new file:   src/components/Item/img/delete.svg 	new file:   src/components/Item/img/pencil_icon.svg 	new file:   src/components/Item/img/pencil_icon_active.svg 	new file:   src/components/ItemList/ItemList.js 	new file:   src/components/ItemList/ItemList.module.css 	new file:   src/components/ItemList/img/error_itemlist.svg 	new file:   src/components/RepoList/RepoList.js 	new file:   src/components/RepoList/RepoList.module.css 	new file:   src/components/Repository/Repository.js 	new file:   src/components/Repository/Repository.module.css 	new file:   src/components/Repository/img/star.svg 	new file:   src/components/Repository/img/union (stroke).svg 	new file:   src/components/Todo/Todo.js 	new file:   src/components/Todo/Todo.module.css 	new file:   src/components/TodoFilter/TodoFilter.js 	new file:   src/components/TodoFilter/TodoFilter.module.css 	new file:   src/index.js

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './Contacts.module.css';
+
+const userContacts = {
+  email: 'ivanova.ov_87@mail.ru',
+  phoneNumber: '+7(925)781-76-97'
+};
+
+const Contacts = () => (
+  <div className={styles.wrap}>
+    <a href='mailto:ivanova.ov_87@mail.ru' className={styles.email_link}>
+      { userContacts.email }
+    </a>
+    <a href='tg://resolve?domain=ivanova_2807' className={styles.phone_link}>
+      { userContacts.phoneNumber }
+    </a>
+    <div className={styles.social_networks}>
+      <a
+        href='https://github.com/Lesenok2807'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <div className={styles.github}></div>
+      </a>
+      <a
+        href='https://vk.com/id40188189'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <div className={styles.vkontakte}></div>
+      </a>
+      <a
+        href='https://www.instagram.com/damon_musk/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <div className={styles.instagram}></div>
+      </a>
+      <a
+        href='https://www.facebook.com/damon.musk'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <div className={styles.facebook}></div>
+      </a>
+    </div>
+  </div>
+);
+
+export default Contacts;
